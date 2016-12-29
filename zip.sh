135f5b29a97259993cc2f3ba6728e63f3944d3f4
ya0shen9 <417129395@qq.com>
2016-12-29 18:49:17 +0800

    Update the patch for version Flyme 5.1.12.23

--- zip.sh
+++ zip.sh
@@ -9,29 +9,61 @@
 
 #!/bin/bash
 
-echo "in $0"
-OUT_DIR=/media/ya0shen9/290a963a-1a45-4e8a-a273-333c15380e87/FlymeOS-5/devices/ZTE-Grand-S-II-LTE/out
-OTHER_DIR=/media/ya0shen9/290a963a-1a45-4e8a-a273-333c15380e87/FlymeOS-5/devices/ZTE-Grand-S-II-LTE/other
+unzip -n ./out/flyme_ZTE-Grand-S-II-LTE_ya0shen9_5.1.12.23R.zip -d ./out/temp
 
-rm -rf $OUT_DIR/temp
-mkdir $OUT_DIR/temp
-unzip -n $OUT_DIR/flyme_ZTE-Grand-S-II-LTE_ya0shen9_5.1.12.16R.zip -d $OUT_DIR/temp
+echo "准备打包"
+#cp -r ./other/firmware ./out/temp
+#cp -r ./other/data ./out/temp
+#cp -r ./other/supersu ./out/temp
+#cp ./other/xposed/app/DisableFlagSecure.apk ./out/temp/data/app
+#cp ./other/xposed/app/MeiHua.apk ./out/temp/data/app
+#cp ./other/xposed/app/MeiHuaUI.apk ./out/temp/data/app
+cp ./other/updater-script ./out/temp/META-INF/com/google/android/updater-script
 
-cp -r $OTHER_DIR/xposed/app $OUT_DIR/temp/data
-cp -r $OTHER_DIR/data/app $OUT_DIR/temp/data
-cp -r $OTHER_DIR/firmware $OUT_DIR/temp
-#cp -r $OTHER_DIR/supersu ./out/temp
-cp -r $OTHER_DIR/updater-script $OUT_DIR/temp/META-INF/com/google/android/updater-script
+#mv -f ./out/temp/system/app/AlarmClock ./out/temp/data/system_app
+#mv -f ./out/temp/system/app/AppCenter ./out/temp/data/system_app
+#mv -f ./out/temp/system/app/Calculator ./out/temp/data/system_app
+#mv -f ./out/temp/system/app/Calendar ./out/temp/data/system_app
+#mv -f ./out/temp/system/app/Camera ./out/temp/data/system_app
+#mv -f ./out/temp/system/app/FileManager ./out/temp/data/system_app
+#mv -f ./out/temp/system/app/GameCenter ./out/temp/data/system_app
+#mv -f ./out/temp/system/app/Life ./out/temp/data/system_app
+#mv -f ./out/temp/system/app/Map ./out/temp/data/system_app
+#mv -f ./out/temp/system/app/Painter ./out/temp/data/system_app
+#mv -f ./out/temp/system/app/Reader ./out/temp/data/system_app
 
-if [ -e $OUT_DIR/merged_target_files/A1 ];then
-cd $OUT_DIR/temp
-zip -r ../A1-flyme_ZTE-Grand-S-II-LTE_ya0shen9_5.1.12.16R.zip *
+#mv -f ./out/temp/system/priv-app/Browser ./out/temp/data/system_priv-app
+#mv -f ./out/temp/system/priv-app/EBook ./out/temp/data/system_priv-app
+#mv -f ./out/temp/system/priv-app/Gallery ./out/temp/data/system_priv-app
+#mv -f ./out/temp/system/priv-app/Music ./out/temp/data/system_priv-app
+#mv -f ./out/temp/system/priv-app/Video ./out/temp/data/system_priv-app
 
-elif [ -e $OUT_DIR/merged_target_files/A2 ];then
-cd $OUT_DIR/temp
-zip -r ../A2-flyme_ZTE-Grand-S-II-LTE_ya0shen9_5.1.12.16R.zip *
+echo "纯净包"
+cd ./out/temp
+zip -r ../full-flyme_ZTE-Grand-S-II-LTE_ya0shen9_5.1.12.23R.zip *
+cd ../..
 
-else
-cd $OUT_DIR/temp
-zip -r ../full-flyme_ZTE-Grand-S-II-LTE_ya0shen9_5.1.12.16R.zip *
-fi
+echo "奇兔包"
+cp -r ./other/qitu/qitu ./out/temp/system/system/app
+cp -r ./other/qitu/xunfeishurufa ./out/temp/system/system/app
+cp -r ./other/qitu/baidushoujiweishi ./out/temp/system/system/app
+cp -r ./other/qitu/sogouliulanqi ./out/temp/system/system/app
+cp -r ./other/qitu/wandoujia ./out/temp/system/system/app
+#cp -r ./other/qitu/yingyongzhongxin ./out/temp/system/system/app
+cd ./out/temp
+zip -r ../A2-flyme_ZTE-Grand-S-II-LTE_ya0shen9_5.1.12.23R.zip *
+cd ../..
+
+#echo "速推包"
+#cp -r ./other/sutui/sutui ./out/temp/system/app
+#cp -r ./other/sutui/xunfeishurufa ./out/temp/system/app
+#cp ./other/sutui/liulanqi.apk ./out/temp/data/app/liulanqi.apk
+#cp ./other/sutui/yingyongzhongxin.apk ./out/temp/data/app/yingyongzhongxin.apk
+#cp ./other/sutui/360weishi.apk ./out/temp/data/app/360weishi.apk
+#cp ./other/sutui/jinritoutiao.apk ./out/temp/data/app/jinritoutiao.apk
+#cd ./out/temp
+#zip -r ../B1-flyme_ZTE-Grand-S-II-LTE_ya0shen9_5.1.12.23R.zip *
+#cd ../..
+
+#echo "删除临时文件"
+#rm -r ./out/temp
